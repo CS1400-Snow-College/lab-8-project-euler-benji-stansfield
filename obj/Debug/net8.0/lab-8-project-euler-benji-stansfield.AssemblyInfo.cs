cb1f5297807bebbadc18765adc2c7bc8fd87843e
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-03-25 15:36:33 -0600

    Problem 2 solved

--- obj/Debug/net8.0/lab-8-project-euler-benji-stansfield.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-8-project-euler-benji-stansfield.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3297548dffd41039ace27e03ce471478f50cc7d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeee85fe95c4a994cc558990999e1de8de3f43d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
