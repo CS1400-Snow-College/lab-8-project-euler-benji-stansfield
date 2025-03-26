51f7e7737d596f6303b09b84159b960ad1a2eee8
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-03-26 01:39:37 -0600

    Problem 3 solved

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ int eulerProblem = userChoice switch
 {
     1 => MultiplesOf3Or5(1000),
     2 => EvenFibonacciNumbers(4000000),
-    /*3 => SmallestMultiple(),
-    4 => Prime10_001(),*/
+    3 => SmallestMultiple(),
+    /*4 => Prime10_001(),*/
 };
 
 /*Multiples of 3 or 5 problem*/
@@ -65,4 +65,25 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 
     Console.WriteLine($"Answer: {sum}");
     return sum;
+}
+
+/*Smallest multiple problem*/
+static int SmallestMultiple()
+{
+    Console.WriteLine(@"2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?");
+
+    int num = 1;
+    while(true)
+    {
+        if (num % 2 == 0 && num % 3 == 0 && num % 4 == 0 && num % 5 == 0 && num % 6 == 0 && num % 7 == 0 && num % 8 == 0 && num % 9 == 0 && num % 10 == 0 && num % 11 == 0 && num % 12 == 0 && num % 13 == 0 && num % 14 == 0 && num % 15 == 0 && num % 16 == 0 && num % 17 == 0 && num % 18 == 0 && num % 19 == 0 && num % 20 == 0)
+        {
+            Console.WriteLine($"Answer: {num}");
+            return num;
+        }
+
+        num++;
+    }
+
 }
--- obj/Debug/net8.0/lab-8-project-euler-benji-stansfield.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-8-project-euler-benji-stansfield.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeee85fe95c4a994cc558990999e1de8de3f43d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1f5297807bebbadc18765adc2c7bc8fd87843e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-8-project-euler-benji-stansfield")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
